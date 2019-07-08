f012f9f9ee6a4c8ce8c91d4fe4f7b45d4d65ce27
Dean <dean.zhang@shopee.com>
2019-07-08 19:25:16 +0800

    Update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/airdb/passport
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/airdb/sailor v0.0.0-20190703060937-de3d2cb7d1a7 // indirect
+	github.com/gin-gonic/gin v1.4.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/airdb/passport/web"
+import (
+	"fmt"
+	"github.com/airdb/passport/web"
+	"github.com/airdb/sailor/config"
+	// "github.com/airdb/sailor/dbutils"
+)
 
 func main() {
+	fmt.Println(config.GetEnv())
+	fmt.Println(config.Get("wechat"))
+	fmt.Println(config.GetString("wechat.api"))
+	for _, a := range config.GetDatabases() {
+		fmt.Println(a)
+	}
+	// fmt.Println(config.GetDatabases())
+	
+	// dbutils.()
+
 	web.Run()
 }
