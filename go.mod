cb463e58204ec02cd756fef51b5628324ae007eb
YiNo <dean@airdb.com>
2019-07-10 21:42:41 +0800

    update docker

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/airdb/passport
 go 1.12
 
 require (
-	github.com/airdb/sailor v0.0.0-20190709145427-09c8e626a7a5 // indirect
+	github.com/airdb/sailor v0.0.0-20190709160556-6086d6169449 // indirect
 	github.com/gin-gonic/gin v1.4.0
 )
--- web/handlers/wechat.go
+++ web/handlers/wechat.go
@@ -20,6 +20,6 @@ func WechatLogin(c *gin.Context) {
 }
 
 func Auth(c *gin.Context) {
-	u := "https://open.weixin.qq.com/connect/qrconnect?appid=wxbdc5610cc59c1631&redirect_uri=https%3A%2F%2Fpassport.yhd.com%2Fwechat%2Fcallback.do&response_type=code&scope=snsapi_login&state=3d6be0a4035d839573b04816624a415e#wechat_redirect"
+	u := "https://open.weixin.qq.com/connect/qrconnect?appid=wxb2a310377819aebd&redirect_uri=https://mina.baobeihuijia.com/dev/lastest/wechatapi/wechat/login&response_type=code&scope=snsapi_login&state=bbhj"
 	c.Redirect(307, u)
 }
