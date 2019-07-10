e9937a5c5df491c6c7e13b2eb4aad608a14cd3b4
YiNo <dean@airdb.com>
2019-07-10 23:31:43 +0800

    update docker

--- web/handlers/wechat.go
+++ web/handlers/wechat.go
@@ -15,13 +15,16 @@ import (
 // @Success 200 {object} vo.ListenerResp
 // @Router /wechat/login [post]
 func WechatLogin(c *gin.Context) {
+	code := c.PostForm("code")
+	fmt.Println("xxxx", code)
+
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
 }
 
 func Auth(c *gin.Context) {
-	u := "https://open.weixin.qq.com/connect/qrconnect?appid=wxb2a310377819aebd&redirect_uri=https://mina.baobeihuijia.com/dev/lastest/wechatapi/wechat/login&response_type=code&scope=snsapi_login&state=bbhj"
+	u := "https://open.weixin.qq.com/connect/qrconnect?appid=wxb2a310377819aebd&redirect_uri=https://mina.baobeihuijia.com/auth/v1/wechat/login&response_type=code&scope=snsapi_login&state=bbhj"
 	fmt.Println("xxx rewrite")
-	c.Redirect(302, u)
+	c.Redirect(307, u)
 }
