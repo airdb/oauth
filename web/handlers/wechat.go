0ad1535e9ce994254c43a34a679da79293104847
YiNo <dean@airdb.com>
2019-07-13 18:03:15 +0800

    add gorm

--- web/handlers/wechat.go
+++ web/handlers/wechat.go
@@ -17,15 +17,15 @@ import (
 // @Success 200 {object} vo.ListenerResp
 // @Router /wechat/login [post]
 func WechatLogin(c *gin.Context) {
-	code := c.PostForm("code")
-	fmt.Println("request_method_is", c.Request.Method)
+	code := c.Param("code")
+	fmt.Println("request_method_is", c.Request.Method,code)
 	var logincode vo.LoginReq
 	if err := c.ShouldBindQuery(&logincode); err != nil {
 		fmt.Println("xxxx", err)
 	}
 
-	fmt.Println("code is ", code, logincode.Code)
-	bo.GetWechatAccessToken(logincode.Code)
+	fmt.Println("code is ", logincode.Code)
+	bo.GetWechatAccessToken(code)
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
