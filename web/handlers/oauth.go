881e239b64e36464428f83efdb2933fcdb300b80
YiNo <dean@airdb.com>
2019-11-20 23:53:49 +0800

    update

--- web/handlers/oauth.go
+++ web/handlers/oauth.go
@@ -56,15 +56,14 @@ func Redirect(c *gin.Context) {
 	providerData := vo.QueryProvider(provider)
 
 	authURL, err := NewDispatcher().New().Driver(provider).Redirect(
-			providerData.ClientID,
-			providerData.ClientSecret,
-			providerData.RedirectURI,
-		)
+		providerData.ClientID,
+		providerData.ClientSecret,
+		providerData.RedirectURI,
+	)
 	if err != nil {
 		c.Redirect(http.StatusFound, "/")
 	}
 	// authURL := fmt.Sprintf("%s?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_login&state=bbhj", providerData.URL, providerData.ClientID, providerData.RedirectURI)
-
 
 	fmt.Println(authURL)
 	c.Redirect(http.StatusFound, authURL)
